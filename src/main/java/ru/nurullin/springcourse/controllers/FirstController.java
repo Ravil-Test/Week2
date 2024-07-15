26f9046f365ed57f0eac061f73fd757a51f09a68
Titran <kolanho3@gmail.com>
2024-07-15 18:57:53 +0300

    Переходит на страницу Home с параметром Name

--- src/main/java/ru/nurullin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/nurullin/springcourse/controllers/FirstController.java
@@ -1,14 +1,21 @@
 package ru.nurullin.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.PrintWriter;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/home")
-    public String homePage() {
+    public String homePage(@RequestParam(name = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+
         return "first/home";
     }
 
