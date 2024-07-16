a6944f4449917837fda6957b87b31419ff5a0ccc
Titran <kolanho3@gmail.com>
2024-07-16 07:53:53 +0300

    Задание "Каталог оружия"

---
+++ src/main/java/ru/nurullin/springcourse/models/Person.java
@@ -0,0 +1,51 @@
+package ru.nurullin.springcourse.models;
+
+public class Person {
+    private int id;
+    private String name;
+    private String type;
+    private int capacity;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, String type, int capacity) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
