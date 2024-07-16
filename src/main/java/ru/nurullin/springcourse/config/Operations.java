2d67bbae0ba7e241c4ac901c4bb8bada8a21ceae
Titran <kolanho3@gmail.com>
2024-07-16 18:59:57 +0300

    Задание "Калькулятор"

---
+++ src/main/java/ru/nurullin/springcourse/config/Operations.java
@@ -0,0 +1,5 @@
+package ru.nurullin.springcourse.config;
+
+public enum Operations {
+    ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION
+}
--- src/main/java/ru/nurullin/springcourse/controllers/FirstController.java
+++ src/main/java/ru/nurullin/springcourse/controllers/FirstController.java
@@ -3,30 +3,59 @@ package ru.nurullin.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.nurullin.springcourse.config.Operations;
 
-import javax.servlet.annotation.WebServlet;
-import java.io.PrintWriter;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
-    @GetMapping("/home")
-    public String homePage(@RequestParam(name = "name", required = false) String name, Model model) {
-        model.addAttribute("name", name);
-
+    @GetMapping
+    public String home(){
         return "first/home";
     }
 
-    @GetMapping("/hello")
-    public String helloPage() {
-        return "first/hello";
-    }
+    @PostMapping
+    public String home(
+            @RequestParam(name = "first_number", defaultValue = "0") String first_number,
+            @RequestParam(name = "second_number", defaultValue = "0") String second_number,
+            @RequestParam(name = "operation", required = false) Operations operation,
+            @RequestParam(name = "result_field", required = false, defaultValue = "") String result_field,
+            Model model) {
 
-    @GetMapping("/goodbye")
-    public String hello() {
-        return "first/goodbye";
+        StringBuilder builder = new StringBuilder();
+        builder.append(result_field);
+
+        String result = result_field + "\n";
+        try {
+            int first = Integer.parseInt(first_number);
+            int second = Integer.parseInt(second_number);
+
+            switch (operation) {
+                case ADDITION: result = first + second + "";
+                    break;
+                case SUBTRACTION: result = first - second + "";
+                    break;
+                case MULTIPLICATION: result = first * second + "";
+                    break;
+                case DIVISION: result = first / second + "";
+                    break;
+                default: result = "Ошибка ввода данных";
+            }
+        } catch (Exception e) {
+            result = "Ошибка ввода данных";
+        }
+
+        builder.insert(0, result + "\n");
+
+        model.addAttribute("operation", operation);
+        model.addAttribute("first_number", first_number);
+        model.addAttribute("second_number", second_number);
+        model.addAttribute("result_field", builder.toString());
+        return "first/home";
     }
 
 }
